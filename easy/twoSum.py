2008cff07b54133164494b090ab01b96727671c8
Satish Patel <SSJ.SATISH@GMAIL.COM>
2019-07-29 11:37:33 +0530

    initial commit

---
+++ easy/twoSum.py
@@ -0,0 +1,30 @@
+# Approach #01
+'''
+    Using 2 loops to check every combination if there is a pair whose sum equals to the given number.
+    Time: O(n^2)
+    Space: O(1)
+'''
+def twoSum(ar, k):
+
+    return []
+
+
+# Approach #02
+'''
+    Using a hash/dictionary to lookup if
+    Time: O(n)
+    Space: O(n)
+'''
+def twoSumUsingDictionary(ar, k):
+    
+    return []
+
+# Approach #03
+'''
+    Using a sort function to sort the given list first then use binary search method.
+    Time: O(n log(n))
+    Space: O(1)
+'''
+def twoSumUsingSort(ar, k):
+
+    return []
