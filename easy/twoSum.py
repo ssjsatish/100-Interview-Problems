92848330dae243d5d59853cfa7d1c935c23e3a17
Satish Patel <SSJ.SATISH@GMAIL.COM>
2019-12-18 17:13:02 +0530

    twoSum using 2 loops to find the pair if sum of those pair equals the targetSum

--- easy/twoSum.py
+++ easy/twoSum.py
@@ -5,6 +5,10 @@
     Space: O(1)
 '''
 def twoSum(ar, k):
+    for i in range(len(ar)-1):
+        for j in range(i+1, len(ar)):
+            if((ar[i]+ar[j])==k):
+                return sorted([ar[i],ar[j]]) 
     return []
 
 
