c0b5898ec8bf086fccce5e67e31267b12ce2b4a8
Satish Patel <SSJ.SATISH@GMAIL.COM>
2020-01-31 12:59:10 +0530

    3 largest element

--- easy/3largest.py
+++ easy/3largest.py
@@ -1,7 +1,9 @@
+# O(nlogn) Simple but not the best
 def findThreeLargestNumbers(array):
     array.sort()
 	return array[-3:]
 
+# O(n) simple and a lot better
 def threeLargestNumbers(a):
     ans = [None]*3
     # traverse and keep track of 3 elements
