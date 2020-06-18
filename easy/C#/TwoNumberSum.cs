9d348bd0b7c5d302672b58f797ae4673f0945e2c
Gupteshwari Sahu <gupteshwari@gmail.com>
2020-06-18 23:18:13 +0530

    initial commit

---
+++ easy/C#/TwoNumberSum.cs
@@ -0,0 +1,11 @@
+public class Program {
+	public static int[] TwoNumberSum(int[] array, int targetSum) {
+		var d = new Dictionary<int,int>();
+		for (int i = 0; i < array.Length; i++)
+		{
+			
+			
+		}
+		return null;
+	}
+}
