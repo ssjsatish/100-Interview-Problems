8e41838a979a03d2ab2a4ed9f9519ad89553dd98
Gupteshwari Sahu <gupteshwari@gmail.com>
2020-06-19 22:49:28 +0530

    Two Sum in java

--- easy/C#/TwoSum.cs
+++ easy/C#/TwoSum.cs
@@ -1,9 +1,10 @@
-public class Program {
+public class TwoSum {
 	public static int[] TwoNumberSum(int[] array, int targetSum) {
 		var d = new Dictionary<int,int>();
 		for (int i = 0; i < array.Length; i++)
 		{
-			
+			int temp = targetSum - array[i];
+			if ()
 			
 		}
 		return null;
--- easy/java/TwoSum.java
+++ easy/java/TwoSum.java
@@ -1,5 +1,15 @@
-public class TwoSum {
-    public static void main(String [] args) {
+import java.util.*;
 
+class Program {
+  public static int[] twoNumberSum(int[] array, int targetSum) {
+ int len= array.length;
+		for(int i=0;i<len-1;i++){
+    for(int j=i+1;j<len;j++){
+        if(array[i]+array[j]==targetSum){
+          return new int[] { array[i],array[j] };
+        }
     }
+}
+		return new int[] {};
+  }
 }
