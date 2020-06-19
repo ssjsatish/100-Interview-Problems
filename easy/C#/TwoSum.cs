e8e270679c659c270b7def9b104e15914af6ddd2
Gupteshwari Sahu <gupteshwari@gmail.com>
2020-06-19 23:35:23 +0530

    reverted the py code

--- easy/C#/TwoSum.cs
+++ easy/C#/TwoSum.cs
@@ -1,12 +1,6 @@
 public class TwoSum {
 	public static int[] TwoNumberSum(int[] array, int targetSum) {
-		var d = new Dictionary<int,int>();
-		for (int i = 0; i < array.Length; i++)
-		{
-			int temp = targetSum - array[i];
-			if ()
-			
-		}
+		
 		return null;
 	}
 }
